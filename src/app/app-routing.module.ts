ef46fe1de5c77b55dfcbf4a7d6141b3721084bce
princy <princydhaduk4@gmail.com>
2024-08-31 14:55:14 -0400

    add header and footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'home',
+    pathMatch:'full'
+  },
+  {
+    path:'home',
+    component:LandingComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,scss}"],
+  theme: {
+    extend: {
+      colors:{
+        'primary':"#EAA636",
+        secondary:"#FDF5EB",
+        t1:"#545454",
+      }
+    },
+  },
+  plugins: [],
+}
